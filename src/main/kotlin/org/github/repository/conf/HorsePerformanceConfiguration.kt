9a0bfa398dc2f794f625da6bd92f1a2b66033719
Kim Toivonen <kim.toivonen@outlook.com>
2022-04-17 14:52:14 +0300

    Fix mongo configuration

--- src/main/kotlin/org/github/repository/conf/HorsePerformanceConfiguration.kt
+++ src/main/kotlin/org/github/repository/conf/HorsePerformanceConfiguration.kt
@@ -3,7 +3,7 @@ package org.github.repository.conf
 import io.micronaut.context.annotation.ConfigurationProperties
 import io.micronaut.core.naming.Named
 
-@ConfigurationProperties("api_endpoints")
+@ConfigurationProperties("horse_perf")
 interface HorsePerformanceConfiguration : Named {
     val collection: String
 }
